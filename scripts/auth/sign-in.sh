0875619f7550c2c7cb1682b0530f2e3469d3a8c6
kailinc <kailinc@bu.edu>
2017-08-05 14:22:56 -0400

    Initialized Files

---
+++ scripts/auth/sign-in.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+API="http://localhost:4741"
+URL_PATH="/sign-in"
+EMAIL='muda'
+PASSWORD='bish'
+curl "${API}${URL_PATH}" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data '{
+    "credentials": {
+      "email": "'"${EMAIL}"'",
+      "password": "'"${PASSWORD}"'"
+    }
+  }'
+
+echo
---
+++ scripts/auth/sign-up.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+API="http://localhost:4741"
+URL_PATH="/sign-up"
+EMAIL='muda'
+PASSWORD='bish'
+curl "${API}${URL_PATH}" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data '{
+    "credentials": {
+      "email": "'"${EMAIL}"'",
+      "password": "'"${PASSWORD}"'",
+      "password_confirmation": "'"${PASSWORD}"'"
+    }
+  }'
+
+echo
