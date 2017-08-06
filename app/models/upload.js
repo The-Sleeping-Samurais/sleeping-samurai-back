6b18887852491de7822cfb972456231037781096
azinck94 <Andrew_zinck@student.uml.edu>
2017-08-06 15:58:40 -0400

    Adds user authentication and ownership for uploads
    -referenced example controller
    -set before action of setUser for only index and show
    -set before action of authenticate for only update, delete and create
    -passed request into AWSupload
    -set virtuals for upload
    -Worked with Kai!

--- app/models/upload.js
+++ app/models/upload.js
@@ -3,21 +3,39 @@
 const mongoose = require('mongoose')
 
 const uploadSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  // location of the hosted file
-  url: {
-    type: String,
-    required: true
-  }
-}, {
-  timestamps: true
-})
+      name: {
+        type: String,
+        required: true
+      },
+      _owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+      },
+    url: {
+      type: String,
+      required: true
+      }
+    }, {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+      transform: function(doc, ret, options) {
+        const userId = (options.user && options.user._id) || false
+        ret.editable = userId && userId.equals(doc._owner)
+        return ret
+      }
+    }
+  })
+
+//This will define virtuals
+  // uploadSchema.virtual('length').get(function length () {
+  //   return this.text.length
+  // })
 
 // constructor function for creating uploads
 // uses uploadSchema as its schema
 const Upload = mongoose.model('Upload', uploadSchema)
+
 
 module.exports = Upload
